9b053a5471ef3a59f0ad791b9603d9b10fc73d6f
Su Nguyen <nguyenvansu51199@gmail.com>
2023-07-27 11:42:19 +0700

    july-27

--- js/home.js
+++ js/home.js
@@ -1,40 +1,52 @@
 import postApi from './api/postsApi'
-import { setTextContent } from './utils'
+import { setTextContent, truncateText } from './utils'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+// to use fromNow function
+dayjs.extend(relativeTime)
 
 function createPostElement(post) {
   if (!post) return
 
-  try {
-    // find and clone template
-    const postTemplate = document.getElementById('postTemplate')
-    if (!postTemplate) return
+  // find and clone template
+  const postTemplate = document.getElementById('postTemplate')
+  if (!postTemplate) return
 
-    const liElement = postTemplate.content.firstElementChild.cloneNode(true)
-    if (!liElement) return
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true)
+  if (!liElement) return
 
-    // update title, description, author, thumbnail
-    // const titleElement = liElement.querySelector('[data-id="title"]')
-    // if (titleElement) titleElement.textContent = post.title
-    setTextContent(liElement, '[data-id="title"]', post.title)
-    setTextContent(liElement, '[data-id="author"]', post.author)
-    setTextContent(liElement, '[data-id="description"]', post.description)
+  // update title, description, author, thumbnail
+  // const titleElement = liElement.querySelector('[data-id="title"]')
+  // if (titleElement) titleElement.textContent = post.title
+  setTextContent(liElement, '[data-id="title"]', post.title)
+  setTextContent(liElement, '[data-id="author"]', post.author)
+  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100))
 
-    // const authorElement = liElement.querySelector('[data-id="author"]')
-    // if (authorElement) authorElement.textContent = post.author
+  // const authorElement = liElement.querySelector('[data-id="author"]')
+  // if (authorElement) authorElement.textContent = post.author
 
-    // const descriptionElement = liElement.querySelector('[data-id="description"]')
-    // if (descriptionElement) descriptionElement.textContent = post.description
+  // const descriptionElement = liElement.querySelector('[data-id="description"]')
+  // if (descriptionElement) descriptionElement.textContent = post.description
 
-    const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
-    if (thumbnailElement) thumbnailElement.src = post.imageUrl
+  // calculate timespan
+  console.log(dayjs(post.updateAt).fromNow())
+  setTextContent(liElement, '[data-id="timeSpan"]', dayjs(post.updateAt).fromNow())
 
-    // if
-    // attach events
+  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
+  if (thumbnailElement) {
+    thumbnailElement.src = post.imageUrl
 
-    return liElement
-  } catch (error) {
-    console.log('failed to create post item', error)
+    thumbnailElement.addEventListener('error', () => {
+      console.log('load image error --> use default placeholder')
+      thumbnailElement.src = 'https://placehold.co/600x400/png'
+    })
   }
+
+  // if
+  // attach events
+
+  return liElement
 }
 
 function renderPostList(postList) {
@@ -50,13 +62,63 @@ function renderPostList(postList) {
   })
 }
 
+function handleFilterChange(filterName, filerValue) {
+  // update query params
+  const url = new URL(window.location)
+  url.searchParams.set(filterName, filerValue)
+  history.pushState({}, '', url)
+
+  //  fetch API
+  // re-render post List
+}
+
+function handlePrevClick(e) {
+  e.preventDefault()
+  console.log('prev click')
+}
+function handleNextClick(e) {
+  e.preventDefault()
+  console.log('next click')
+}
+
+function initPagination() {
+  // bind click event for prev/next link
+  const ulPagination = document.getElementById('pagination')
+  if (!ulPagination) return
+
+  // add click event for prev link
+  const prevLink = ulPagination.firstElementChild?.firstElementChild
+  if (prevLink) {
+    prevLink.addEventListener('click', handlePrevClick)
+  }
+
+  const nextLink = ulPagination.lastElementChild?.firstElementChild
+  if (nextLink) {
+    nextLink.addEventListener('click', handleNextClick)
+  }
+}
+
+function initURL() {
+  const url = new URL(window.location)
+
+  // update search params if needed
+  if (!url.searchParams.get('_page')) url.searchParams.set('_page', 1)
+  if (!url.searchParams.get('_limit')) url.searchParams.set('_limit', 6)
+
+  history.pushState({}, '', url)
+}
+
 ;(async () => {
   try {
-    const queryParams = {
-      _page: 1,
-      _limit: 6,
-    }
+    initPagination()
+    initURL()
+
+    const queryParams = new URLSearchParams(window.location.search)
+    // set default query if not exited
+    console.log(queryParams.toString())
+
     const { data, pagination } = await postApi.getAll(queryParams)
+    console.log(data)
     renderPostList(data)
   } catch (error) {
     console.log('get all failed', error)
